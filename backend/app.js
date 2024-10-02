df7cf270f4ec5e2e6be117e0380685ad4a6c8763
pavan-crescentek <pavan@outrightsolutions.net>
2024-10-02 15:28:15 +0530

    cors added

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,18 @@ const cors = require('cors');
 const { default: mongoose } = require('mongoose');
 var http = require('http').Server(app);
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
+  next();
+});
+
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors());
