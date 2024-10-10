ce4bbdffab494f72058f289b4771834328adf02a
pavan-crescentek <pavan@outrightsolutions.net>
2024-10-10 14:32:00 +0530

    fix: increase file size

--- backend/app.js
+++ backend/app.js
@@ -20,8 +20,8 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
 app.use(cors());
 
 app.use('/', router);
